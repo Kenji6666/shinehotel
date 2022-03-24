2a45f5776e8bc969c020b6bde3237ed3862f7c44
Kenji <88219548+Kenji6666@users.noreply.github.com>
2022-03-24 09:20:23 +0700

    Add files via upload

---
+++ hotel.sql
@@ -0,0 +1,252 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 23 Mar 2022 pada 04.44
+-- Versi server: 10.4.13-MariaDB
+-- Versi PHP: 7.4.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hotel`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_fasilitas`
+--
+
+CREATE TABLE `tb_fasilitas` (
+  `no` int(3) NOT NULL,
+  `nama` text NOT NULL,
+  `gambar` varchar(255) NOT NULL,
+  `keterangan` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tb_fasilitas`
+--
+
+INSERT INTO `tb_fasilitas` (`no`, `nama`, `gambar`, `keterangan`) VALUES
+(1, 'Swiming Pool', 'pool.jpg', 'Nikmati hari Anda berenang di kolam renang terbuka dengan tekanan lembut untuk pijat relaksasi yang menghadap ke lembah yang indah, kami menjamin kebersihan air kolam di Shine Hotel<br><br>Open : 06.00 <br>Close : 21.00'),
+(2, 'Restaurant', 'dapur.jfif', 'Terletak di atas keindahan kota yang memberi Anda angin segar. Puaskan momen Anda bersama orang terkasih dengan menikmati makan malam romantis restaurant kami yang dilengkapi dengan pemandangan kota yang menakjubkan.\r\n<br>\r\n<br>\r\nBreakfast :     06.00 - 11.00<br>\r\nLunch	:	12.00 - 14.00<br>\r\nDinner	:	18.30 - 22.00<br>'),
+(3, 'View', 'window.jpg', 'Hotel kami terdapat di tengah tengah keindahan kota sehingga membuat anda dapat secara langsung melihat pemandangan menakjubkan dari jendela'),
+(4, 'Lahan Parkir', 'parkir.jpeg', 'Kami menyediakan lahan parkir untuk anda yang membawa kendaraan pribadi saat berada di Shine hotel, ditambah dengan lahan yang luas dan terhindar dari terik matahari atau pun guyuran hujan demi menambah kepuasan anda'),
+(5, 'Food', 'food2.jpg', 'Setiap hidangan dibuat dengan indah menjadi sebuah karya seni, dijamin akan memberikan cita rasa yang luar biasa. Suasana romantis ini akan memberi Anda suasana berbeda sambil menikmati masakan Asia dan Eropa terbaik kami. ');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_fasilitas_kamar`
+--
+
+CREATE TABLE `tb_fasilitas_kamar` (
+  `id` int(2) NOT NULL,
+  `tipe_kamar` text NOT NULL,
+  `fitur` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tb_fasilitas_kamar`
+--
+
+INSERT INTO `tb_fasilitas_kamar` (`id`, `tipe_kamar`, `fitur`) VALUES
+(1, 'Single Room', 'Meja, Kursi, Flat LCD TV, AC'),
+(2, 'Standard Room', 'Meja Kerja, Kursi, Flat LCD TV, AC'),
+(3, 'Superior Room', 'Meja Kerja, Kursi, Flat LCD TV, AC, Minibar, <br> Coffe Maker, Jubah Mandi, Free Smoking'),
+(4, 'Deluxe Room', 'Meja Kerja, Kursi, Flat LCD TV, AC, Coffe Maker, <br> Minibar, Jubah Mandi, Brankas, Free Smoking');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_harga`
+--
+
+CREATE TABLE `tb_harga` (
+  `id_kamar` int(2) NOT NULL,
+  `tipe_kamar` text NOT NULL,
+  `harga` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tb_harga`
+--
+
+INSERT INTO `tb_harga` (`id_kamar`, `tipe_kamar`, `harga`) VALUES
+(1, 'Single Room', 'Rp.500.000,-'),
+(2, 'Standard Room', 'Rp. 800.000,-'),
+(3, 'Superior Room', 'Rp. 1.000.000'),
+(4, 'Deluxe Room', 'Rp. 1.400.000,-');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_pesan`
+--
+
+CREATE TABLE `tb_pesan` (
+  `id_pesan` int(5) NOT NULL,
+  `nama_tamu` text NOT NULL,
+  `telp_tamu` int(20) NOT NULL,
+  `tipe_kamar` text NOT NULL,
+  `tgl_checkin` date NOT NULL,
+  `tgl_checkout` date NOT NULL,
+  `jumlah_tamu` int(5) NOT NULL,
+  `jumlah_kamar` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tb_pesan`
+--
+
+INSERT INTO `tb_pesan` (`id_pesan`, `nama_tamu`, `telp_tamu`, `tipe_kamar`, `tgl_checkin`, `tgl_checkout`, `jumlah_tamu`, `jumlah_kamar`) VALUES
+(5, 'virgi', 89676876, 'standar', '2022-03-08', '2022-03-10', 2, 1),
+(11, 'tst', 345354, 'standar', '2022-03-02', '2022-03-03', 2, 1),
+(12, 'virgi', 896576, 'standar', '2022-03-02', '2022-03-03', 2, 1),
+(13, 'riski', 8989, 'single', '2022-03-20', '2022-03-21', 2, 1),
+(15, 'atalla', 896758, 'single', '2022-03-09', '2022-03-12', 1, 1),
+(16, 'arpan', 8989, 'standar', '2022-03-16', '2022-03-22', 1, 1),
+(17, 'asssa', 896970, 'single', '2022-03-02', '2022-03-03', 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_room`
+--
+
+CREATE TABLE `tb_room` (
+  `id_kamar` int(10) NOT NULL,
+  `tipe_kamar` enum('Single Room','Standar Room','Superior Room','Deluxe Room') NOT NULL,
+  `stok` varchar(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tb_room`
+--
+
+INSERT INTO `tb_room` (`id_kamar`, `tipe_kamar`, `stok`) VALUES
+(1, 'Single Room', '20'),
+(2, 'Standar Room', '35'),
+(3, 'Superior Room', '30'),
+(4, 'Deluxe Room', '25');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_user`
+--
+
+CREATE TABLE `tb_user` (
+  `id_user` int(11) NOT NULL,
+  `nama` text NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `level` enum('admin','petugas','tamu') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tb_user`
+--
+
+INSERT INTO `tb_user` (`id_user`, `nama`, `username`, `password`, `level`) VALUES
+(32, 'virgi', 'virgiawan@gmail.com', '35b635238a58bce6a095ebfaf2416108', 'tamu'),
+(35, 'virgi', 'virgi@gmail.com', '35b635238a58bce6a095ebfaf2416108', 'tamu'),
+(37, 'admin4', 'admin4', 'fc1ebc848e31e0a68e868432225e3c82', 'admin'),
+(39, 'petugas4', 'petugas4', '95c477e4932b3b16500674c18fb6f9a4', 'petugas'),
+(42, 'radit', 'radit@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b', 'tamu');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `tb_fasilitas`
+--
+ALTER TABLE `tb_fasilitas`
+  ADD PRIMARY KEY (`no`);
+
+--
+-- Indeks untuk tabel `tb_fasilitas_kamar`
+--
+ALTER TABLE `tb_fasilitas_kamar`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `tb_harga`
+--
+ALTER TABLE `tb_harga`
+  ADD PRIMARY KEY (`id_kamar`);
+
+--
+-- Indeks untuk tabel `tb_pesan`
+--
+ALTER TABLE `tb_pesan`
+  ADD PRIMARY KEY (`id_pesan`);
+
+--
+-- Indeks untuk tabel `tb_room`
+--
+ALTER TABLE `tb_room`
+  ADD PRIMARY KEY (`id_kamar`);
+
+--
+-- Indeks untuk tabel `tb_user`
+--
+ALTER TABLE `tb_user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_fasilitas`
+--
+ALTER TABLE `tb_fasilitas`
+  MODIFY `no` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_fasilitas_kamar`
+--
+ALTER TABLE `tb_fasilitas_kamar`
+  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_harga`
+--
+ALTER TABLE `tb_harga`
+  MODIFY `id_kamar` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_pesan`
+--
+ALTER TABLE `tb_pesan`
+  MODIFY `id_pesan` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_room`
+--
+ALTER TABLE `tb_room`
+  MODIFY `id_kamar` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_user`
+--
+ALTER TABLE `tb_user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ singleroom.php
@@ -0,0 +1,437 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Single Room</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700|Work+Sans:300,400,700" rel="stylesheet">
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="css/jquery-ui.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="css/animate.css">
+    
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/mediaelement@4.2.7/build/mediaelementplayer.min.css">
+    
+    
+    
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+  
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/style.css">
+    
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+  </head>
+  <body>
+  
+  <div class="site-wrap">
+
+    <div class="site-mobile-menu">
+      <div class="site-mobile-menu-header">
+        <div class="site-mobile-menu-close mt-3">
+          <span class="icon-close2 js-menu-toggle"></span>
+        </div>
+      </div>
+      <div class="site-mobile-menu-body"></div>
+    </div> 
+    <!-- .site-mobile-menu -->
+    
+    
+    <div class="site-navbar-wrap js-site-navbar bg-white">
+      
+      <div class="container">
+        <div class="site-navbar bg-light">
+          <div class="py-1">
+            <div class="row align-items-center">
+              <div class="col-2">
+                <h2 class="mb-0 site-logo"><a href="index.php">Shine</a></h2>
+              </div>
+              <div class="col-10">
+                <nav class="site-navigation text-right" role="navigation">
+                  <div class="container">
+                    
+                    <div class="d-inline-block d-lg-none  ml-md-0 mr-auto py-3"><a href="#" class="site-menu-toggle js-menu-toggle"><span class="icon-menu h3"></span></a></div>
+                    <ul class="site-menu js-clone-nav d-none d-lg-block">
+                      <li>
+                        <a href="index.php">Home</a>
+                      </li>
+                      <li class="has-children active">
+                        <a href="#">Rooms</a>
+                        <ul class="dropdown arrow-top">
+                          <li><a href="singleroom.php">Single Room</a></li>
+                          <li><a href="standardroom.php">Standard Room</a></li>
+                          <li><a href="superiorroom.php">Superior Room</a></li>
+                          <li><a href="deluxeroom.php">Deluxe Room</a></li>
+                        </ul>
+                      </li>
+                      <li><a href="about.php">Fasilitas</a></li>
+                      <a class="btn btn-primary btn-md" href="../login/index.php" role="button">Book Now</a>
+                    </ul>
+                  </div>
+                </nav>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  
+    
+    <div class="slide-one-item home-slider owl-carousel">
+      
+      <div class="site-blocks-cover overlay" style="background-image: url(images/gedung.jfif);" data-aos="fade" data-stellar-background-ratio="0.5">
+        <div class="container">
+          <div class="row align-items-center justify-content-center">
+            <div class="col-md-7 text-center" data-aos="fade">
+              
+              <h1 class="mb-2">Welcome To Shine</h1>
+              <h2 class="caption">Hotel &amp; Resort</h2>
+            </div>
+          </div>
+        </div>
+      </div>  
+
+      <div class="site-blocks-cover overlay" style="background-image: url(images/dapur.jfif);" data-aos="fade" data-stellar-background-ratio="0.5">
+        <div class="container">
+          <div class="row align-items-center justify-content-center">
+            <div class="col-md-7 text-center" data-aos="fade">
+              <h1 class="mb-2">Unique Experience</h1>
+              <h2 class="caption">Enjoy With Us</h2>
+            </div>
+          </div>
+        </div>
+      </div> 
+
+      <div class="site-blocks-cover overlay" style="background-image: url(images/resto.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
+        <div class="container">
+          <div class="row align-items-center justify-content-center">
+            <div class="col-md-7 text-center" data-aos="fade">
+              <h1 class="mb-2">Relaxing Room</h1>
+              <h2 class="caption">Your Room, Your Stay</h2>
+            </div>
+          </div>
+        </div>
+      </div> 
+
+    </div>
+
+
+    <div class="site-section bg-white">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 mt-5 mx-auto text-center mb-1 section-heading">
+            <h2 class="mb-1" style="color: red;">Single Rooms</h2>
+            <hr width="100%" size="50px" color="red" style="align-items: center;"></hr>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-9 mx-auto text-center mb-1">
+            <h4>Single Room dilengkapi dengan salah satu tempat tidur single. Luas kamar 20m². Kamar juga dilengkapi unit AC, kursi yang nyaman, cermin rias, dan kamar mandi. Desain kamar ini mengutamakan kenyamanan yang akan membuat anda melupakan ukuran ruang, sebaiknya tenggelam dalam kenyamanan yang ditawarkan kamar ini. Anda akan menikmati pemandangan kota yang sempurna dengan banyak landmark kota karena hotel kami adalah salah satu lokasi dengan peringkat teratas</h4>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+
+    <div class="site-section block-15">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 mx-auto text-center mb-5 section-heading">
+            <h2>Details</h2>
+            <hr width="50%" size="50px" color="red" style="align-items: center;"></hr>
+          </div>
+        </div>
+
+
+        
+  <div class="row justify-content-center">
+  <table class="table" style="width: 70%;">
+    <tbody>
+      <tr>
+        <td class="text-left">Guest :</td>
+        <td class="text-right">1</td>
+      </tr>
+      <tr>
+        <td class="text-left">Room Size :</td>
+        <td class="text-right">20m²</td>
+      </tr>
+     <tr>
+        <td class="text-left">Bed Size :</td>
+        <td class="text-right">Single</td>
+     </tr>
+     <tr>
+        <td class="text-left">Room Type :</td>
+        <td class="text-right">Single</td>
+     </tr>
+     <tr>
+        <td class="text-left">Fitur Tambahan :</td>
+        <?php
+    include 'koneksi.php';
+    $data = mysqli_query($koneksi,"select fitur from tb_fasilitas_kamar where id = '1'");
+    while($d = mysqli_fetch_array($data)){
+   ?>
+        <td class="text-right"><?php echo $d['fitur']; ?> 
+        <?php
+  }
+  ?></td>
+     </tr>
+    </tbody>
+  </table>
+  </div>
+
+  <div class="row justify-content-center mt-5">
+    <a class="btn btn-primary btn-lg" href="../login/index.php" role="button">Book Now</a>
+  </div>
+
+        <div class="nonloop-block-15 owl-carousel">
+          
+
+            <div class="media-with-text p-md-4">
+              <div class="img-border-sm mb-4">
+                <a href="#" class="popup-vimeo image-play">
+                  <img src="images/singleroom.jfif" alt="" class="img-fluid">
+                </a>
+              </div>
+              <h2 class="heading mb-0"><a href="#">Singlebed </a></h2>
+              <span class="mb-3 d-block post-date">Dec 20th, 2019 &bullet; By <a href="#">Shine Hotel</a></span>
+              <p>Singlebed adalah tempat tidur dengan ukuran 100x200 cm, ini cukup untuk nyaman untuk di pakai oleh 1 orang dewasa, warna kasur dipilih dengan cermat demi kenyamanan Anda</p>
+            </div>
+          
+            
+            <div class="media-with-text p-md-4">
+              <div class="img-border-sm mb-4">
+                <a href="#" class="popup-vimeo image-play">
+                  <img src="images/bathroom.jfif" alt="" class="img-fluid">
+                </a>
+              </div>
+              <h2 class="heading mb-0"><a href="#">Bathroom</a></h2>
+              <span class="mb-3 d-block post-date">Dec 20th, 2019 &bullet; By <a href="#">Shine Hotel</a></span>
+              <p>Kamar mandi pada Single Room di lengkapi dengan shower, wastafel dan dinding kaca, dengan dan kami menjamin kebersihan nya</p>
+            </div>
+
+            <div class="media-with-text p-md-4">
+              <div class="img-border-sm mb-4">
+                <a href="#" class="popup-vimeo image-play">
+                  <img src="images/singleroom.jfif" alt="" class="img-fluid">
+                </a>
+              </div>
+              <h2 class="heading mb-0"><a href="#">Singlebed </a></h2>
+              <span class="mb-3 d-block post-date">Dec 20th, 2019 &bullet; By <a href="#">Shine Hotel</a></span>
+              <p>Singlebed adalah tempat tidur dengan ukuran 100x200 cm, ini cukup untuk nyaman untuk di pakai oleh 1 orang dewasa, warna kasur dipilih dengan cermat demi kenyamanan Anda</p>
+            </div>
+          
+            <div class="media-with-text p-md-4">
+              <div class="img-border-sm mb-4">
+                <a href="#" class="popup-vimeo image-play">
+                  <img src="images/bathroom.jfif" alt="" class="img-fluid">
+                </a>
+              </div>
+              <h2 class="heading mb-0"><a href="#">Bathroom</a></h2>
+              <span class="mb-3 d-block post-date">Dec 20th, 2019 &bullet; By <a href="#">Shine Hotel</a></span>
+              <p>Kamar mandi pada Deluxe Room di lengkapi dengan shower, bathub, wastafel dan dinding kaca, lebih luas dari kamar lain, dan kami menjamin kebersihan nya</p>
+            </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="site-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 mx-auto text-center mb-5 section-heading">
+            <h2 class="mb-5">Hotel Features</h2>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-6 col-md-4 col-lg-3">
+            <div class="text-center p-4 item">
+              <span class="flaticon-pool display-3 mb-3 d-block text-primary"></span>
+              <h2 class="h5">Swimming Pool</h2>
+            </div>
+          </div>
+          <div class="col-sm-6 col-md-4 col-lg-3">
+            <div class="text-center p-4 item">
+              <span class="flaticon-desk display-3 mb-3 d-block text-primary"></span>
+              <h2 class="h5">Hotel Teller</h2>
+            </div>
+          </div>
+          <div class="col-sm-6 col-md-4 col-lg-3">
+            <div class="text-center p-4 item">
+              <span class="flaticon-exit display-3 mb-3 d-block text-primary"></span>
+              <h2 class="h5">Fire Exit</h2>
+            </div>
+          </div>
+          <div class="col-sm-6 col-md-4 col-lg-3">
+            <div class="text-center p-4 item">
+              <span class="flaticon-parking display-3 mb-3 d-block text-primary"></span>
+              <h2 class="h5">Car Parking</h2>
+            </div>
+          </div>
+
+          <div class="col-sm-6 col-md-4 col-lg-3">
+            <div class="text-center p-4 item">
+              <span class="flaticon-hair-dryer display-3 mb-3 d-block text-primary"></span>
+              <h2 class="h5">Hair Dryer</h2>
+            </div>
+          </div>
+
+          <div class="col-sm-6 col-md-4 col-lg-3">
+            <div class="text-center p-4 item">
+              <span class="flaticon-minibar display-3 mb-3 d-block text-primary"></span>
+              <h2 class="h5">Minibar</h2>
+            </div>
+          </div>
+          <div class="col-sm-6 col-md-4 col-lg-3">
+            <div class="text-center p-4 item">
+              <span class="flaticon-drink display-3 mb-3 d-block text-primary"></span>
+              <h2 class="h5">Drinks</h2>
+            </div>
+          </div>
+          <div class="col-sm-6 col-md-4 col-lg-3">
+            <div class="text-center p-4 item">
+              <span class="display-3 mb-3 d-block text-primary">
+                <i class="bi bi-router"></i></span>
+              <h2 class="h5 mt-3">24 Hours Wifi</h2>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="site-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 mx-auto text-center mb-5 section-heading">
+            <h2 class="mb-5">Our Gallery</h2>
+          </div>
+        </div>
+        <div class="row no-gutters">
+          <div class="col-md-6 col-lg-3">
+            <a href="images/gedung.jfif" class="image-popup img-opacity"><img src="images/gedung.jfif" alt="Image" class="img-fluid"></a>
+          </div>
+          <div class="col-md-6 col-lg-3">
+            <a href="images/dapur.jfif" class="image-popup img-opacity"><img src="images/dapur.jpg" alt="Image" class="img-fluid"></a>
+          </div>
+          <div class="col-md-6 col-lg-3">
+            <a href="images/resto.jpg" class="image-popup img-opacity"><img src="images/resto.jpg" alt="Image" class="img-fluid"></a>
+          </div>
+          <div class="col-md-6 col-lg-3">
+            <a href="images/meja.jpg" class="image-popup img-opacity"><img src="images/meja.jpg" alt="Image" class="img-fluid"></a>
+          </div>
+
+          <div class="col-md-6 col-lg-3">
+            <a href="images/food.jpg" class="image-popup img-opacity"><img src="images/food.jpg" alt="Image" class="img-fluid"></a>
+          </div>
+          <div class="col-md-6 col-lg-3">
+            <a href="images/window.jpg" class="image-popup img-opacity"><img src="images/window.jpg" alt="Image" class="img-fluid"></a>
+          </div>
+          <div class="col-md-6 col-lg-3">
+            <a href="images/room.jpg" class="image-popup img-opacity"><img src="images/room.jpg" alt="Image" class="img-fluid"></a>
+          </div>
+          <div class="col-md-6 col-lg-3">
+            <a href="images/pool.jpg" class="image-popup img-opacity"><img src="images/pool.jpg" alt="Image" class="img-fluid"></a>
+          </div>
+
+        </div>
+      </div>
+    </div>
+    
+
+    
+    
+    <footer class="site-footer">
+      <div class="container">
+        
+
+        <div class="row">
+         <div class="col-md-4">
+            <h3 class="footer-heading mb-4 text-white">About</h3>
+            <p>Filosofi kami adalah tentang kenyamanan dan perhatian tamu. Terinspirasi oleh warisan lokal fasilitas yang terus diperbarui dan layanan khusus yang disampaikan dengan senyum dan ketulusan yang selalu selaras dengan lingkungan lokal</p>
+          </div>
+          <div class="col-md-6">
+            <div class="row">
+              <div class="col-md-6">
+                <h3 class="footer-heading mb-4 text-white">Contact Us</h3>
+                  <ul class="list-unstyled">
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Email : shinehotel@gmail.com </a></li>
+                    <li><a href="#">Telp  : (021)-732-654</a></li>
+                  </ul>
+              </div>
+              <div class="col-md-6">
+                <h3 class="footer-heading mb-4 text-white">Our Location</h3>
+                  <ul class="list-unstyled">
+                    <li><a href="#">Jln. Fatmawati No. 17</a></li>
+                    <li><a href="#">Kota Bandung</a></li>
+                    <li><a href="#">Jawa Barat</a></li>
+                  </ul>
+              </div>
+            </div>
+          </div>
+
+          
+          <div class="col-md-2">
+            <div class="col-md-15"><h3 class="footer-heading mb-4 text-white">Social Media</h3></div>
+              <div class="col-md-12">
+                <p>
+                  <a href="#" class="pb-2 pr-2 pl-0"><span class="icon-facebook"></span></a>
+                  <a href="#" class="p-2"><span class="icon-twitter"></span></a>
+                  <a href="#" class="p-2"><span class="icon-instagram"></span></a>
+
+                </p>
+              </div>
+          </div>
+        </div>
+      
+          
+        </div>
+      </div>
+    </footer>
+  </div>
+
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/jquery-ui.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/jquery.countdown.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/bootstrap-datepicker.min.js"></script>
+  <script src="js/aos.js"></script>
+
+  
+  <script src="js/mediaelement-and-player.min.js"></script>
+
+  <script src="js/main.js"></script>
+    
+
+  <script>
+      document.addEventListener('DOMContentLoaded', function() {
+                var mediaElements = document.querySelectorAll('video, audio'), total = mediaElements.length;
+
+                for (var i = 0; i < total; i++) {
+                    new MediaElementPlayer(mediaElements[i], {
+                        pluginPath: 'https://cdn.jsdelivr.net/npm/mediaelement@4.2.7/build/',
+                        shimScriptAccess: 'always',
+                        success: function () {
+                            var target = document.body.querySelectorAll('.player'), targetTotal = target.length;
+                            for (var j = 0; j < targetTotal; j++) {
+                                target[j].style.visibility = 'visible';
+                            }
+                  }
+                });
+                }
+            });
+    </script>
+
+  </body>
+</html>
