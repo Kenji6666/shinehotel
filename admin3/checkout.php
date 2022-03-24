7352428287a0cb5535c2528290a531e4e2e9a80d
Kenji <88219548+Kenji6666@users.noreply.github.com>
2022-03-24 09:27:11 +0700

    Delete checkout.php

--- admin3/checkout.php
+++
@@ -1,8 +0,0 @@
-<?php 
-include 'koneksi.php';
-if (isset($_POST['id_pesan'])) {
-	$id = $_POST['id_pesan'];
-	mysqli_query($koneksi,"UPDATE tb_pesan SET status = 'Check out' where id_pesan = '$id'");
-	header("location:bookinglist.php");
-}
-?>
